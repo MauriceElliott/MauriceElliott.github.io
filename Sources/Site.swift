864dc2efe50ba40aa32c6f75d949d9c37203d0b0
MauriceElliott <mauricemelliott@gmail.com>
2025-11-29 23:28:19 +0000

    Not what I was expecting, but somewhat improved?

--- Sources/Site.swift
+++ Sources/Site.swift
@@ -20,6 +20,7 @@ struct ExampleSite: Site {
     var url = URL(static: "https://codedawa.dev")
     var builtInIconsEnabled = true
 
+    var darkTheme: (any Theme)? { CustomDarkTheme() }
     var author = "Maurice Elliott"
 
     var homePage = Home()
@@ -28,4 +29,24 @@ struct ExampleSite: Site {
     var articlePages: [any ArticlePage] {
         ArticleLayout()
     }
+}
+
+struct CustomDarkTheme: Theme {
+    var colorScheme: ColorScheme = .dark
+
+    var font: Font = Font(
+        name: "Annotation Mono",
+        source: "./../Assets/fonts/Regular.woff2"
+    )
+
+    var headingFont: Font = Font(
+        name: "Annotation Mono",
+        source: "./../Assets/fonts/Regular.woff2"
+    )
+
+    var monospaceFont: Font = Font(
+         name: "Annotation Mono",
+         source: "./../Assets/fonts/Regular.woff2"
+     )
+
 }
