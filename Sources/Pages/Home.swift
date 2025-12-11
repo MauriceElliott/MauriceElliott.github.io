a712179ecf68bba2e07303c9ac69ac992ef53145
MauriceElliott <mauricemelliott@gmail.com>
2025-12-11 22:37:49 +0000

    fixup! updated main page and removed old articles I am no longer proud of

--- Sources/Pages/Home.swift
+++ Sources/Pages/Home.swift
@@ -5,28 +5,23 @@ struct Home: StaticPage {
     var title = "Codedawa"
 
     var body: some HTML {
-        Text("CodeDawa")
+        Text("Code Dawa")
             .font(.title1)
         
         Text("Code Dawa is the cure to my fractured sanity.")
             .font(.lead)
-            .foregroundStyle(.secondary)
+            .foregroundStyle(.primary)
             .margin(.bottom, 30)
-        
-        Text("Posts ->")
+
+        Link("Github", target: "https://github.com/MauriceElliott")
+            .font(.lead)
+            .foregroundStyle(.bodyEmphasis)
+            .margin(.bottom, 30)
+            
+        Link("About Me ->", target: "/posts/2025-12-07-introduction")
             .font(.title2)
             .margin(.top, 20)
             .margin(.bottom, 10)
-        
-        Link("Golden VM Images", target: "/posts/2025-02-17-golden-vm-images/")
-        Text("February 17, 2025")
-            .foregroundStyle(.secondary)
-        
-        Link("Your Pain Is Mine", target: "/posts/2024-10-16-your-pain-is-mine/")
-        Text("October 16, 2024")
-            .foregroundStyle(.secondary)
-        
-        Link("I have no mouth, yet I must scream.", target: "/posts/2024-10-07-welcome/")
         Text("December 7, 2025")
             .foregroundStyle(.secondary)
     }
