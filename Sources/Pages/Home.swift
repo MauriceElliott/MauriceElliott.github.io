47653fa644c863c1ec14836a54b1a6df7c7dda42
MauriceElliott <mauricemelliott@gmail.com>
2025-12-11 22:50:18 +0000

    fixup! fixup! fixup! updated main page and removed old articles I am no longer proud of

--- Sources/Pages/Home.swift
+++ Sources/Pages/Home.swift
@@ -14,11 +14,16 @@ struct Home: StaticPage {
             .margin(.bottom, 30)
 
         Link("Github Projects ->", target: "https://github.com/MauriceElliott")
-            .font(.title3)
+            .font(.lead)
             .foregroundStyle(.secondary)
+            .margin(.bottom, 30)
+        Text("Selection of my favourite github projects from the last few years.")
+            .font(.body)
+            .foregroundStyle(.body)
             .margin(.bottom, 30)
         
         Link("I have no mouth, yet I must scream.", target: "/posts/2025-12-07-introduction")
+            .foregroundStyle(.primary)
             .font(.title3)
             .margin(.top, 20)
             .margin(.bottom, 10)
