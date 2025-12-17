4d72ae6b6fc76a366e81b4e10c41946b10cbc4cb
MauriceElliott <mauricemelliott@gmail.com>
2025-12-17 22:11:02 +0000

    fixup! fixup! fixup! fixup! Added picture...now to start on portfolio page

--- Sources/Pages/Home.swift
+++ Sources/Pages/Home.swift
@@ -13,7 +13,7 @@ struct Home: StaticPage {
 
         Text("<br>")
 
-        Text("Code Dawa is the cure to my fractured sanity.")
+        Text("Code Dawa is the cure to my self sabotage.")
             .font(.body)
             .foregroundStyle(.primary)
             .margin(.bottom, 30)
@@ -23,14 +23,15 @@ struct Home: StaticPage {
             .margin(.bottom, 30)
             .class("float-end")
 
-        Text("My name is Maurice, I am a software engineer, father, creative, and lover of all things sad and desperate.")
+        Text("My name is Maurice, I am a software engineer, father, creative, and lover of all things sad and desperate. This website is not a professional place, just somewhere I feel comfortable exposing a little of my inner being to the wider internet, in the hopes that it makes others feel normal in their own skin. At the same time it is for me mostly, and if I feel better after posting here, it has done its job.")
             .font(.body)
             .margin(.bottom, 100)
 
         Link("Github", target: "https://github.com/MauriceElliott")
-            .font(.lead)
             .foregroundStyle(.primary)
-            .margin(.bottom, 30)
+            .font(.lead)
+            .margin(.top, 20)
+            .margin(.bottom, 10)
 
         Text("<br>")
 
